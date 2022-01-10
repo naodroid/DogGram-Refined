4f9a141bb51ed35bb1ddf6a114d4a974e988fcae
naodroid <naodroid@gmail.com>
2022-01-10 09:16:22 +0900

    Add ImageRepository
    Alternative for ImageManager, adapted Concurrency

---
+++ DogGram/Utilities/UIImage+Ext.swift
@@ -0,0 +1,28 @@
+//
+//  UIImage+Ext.swift
+//  DogGram
+//
+//  Created by nao on 2022/01/10.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIImage {
+    /// return resized image
+    func resize(size _size: CGSize) -> UIImage? {
+        let widthRatio = _size.width / size.width
+        let heightRatio = _size.height / size.height
+        let ratio = widthRatio < heightRatio ? widthRatio : heightRatio
+
+        let resizedSize = CGSize(width: size.width * ratio, height: size.height * ratio)
+
+        UIGraphicsBeginImageContext(resizedSize)
+        draw(in: CGRect(origin: .zero, size: resizedSize))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return resizedImage
+    }
+}
